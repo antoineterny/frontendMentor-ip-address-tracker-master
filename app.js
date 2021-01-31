c082ab574c049cb11859c9b0d6a0cc0334dea9fe
antoineterny <63339704+antoineterny@users.noreply.github.com>
2021-01-31 18:04:00 +0100

    zoom control

--- app.js
+++ app.js
@@ -1,51 +1,50 @@
 const apiKey = "at_ovm8qolz9gi279lgc31pQSSGMzBLs"
-const baseURL =
-	"https://geo.ipify.org/api/v1?apiKey=at_ovm8qolz9gi279lgc31pQSSGMzBLs"
+const baseURL = "https://geo.ipify.org/api/v1?apiKey=at_ovm8qolz9gi279lgc31pQSSGMzBLs"
 
 const form = document.querySelector("form")
-const result = document.getElementById('result')
-const ip = document.getElementById('ip')
-const city = document.getElementById('city')
-const tz = document.getElementById('tz')
-const isp = document.getElementById('isp')
+const result = document.getElementById("result")
+const ip = document.getElementById("ip")
+const city = document.getElementById("city")
+const tz = document.getElementById("tz")
+const isp = document.getElementById("isp")
 
 const defaultLatlng = L.latLng(48.853450342427564, 2.348776314097173)
 const mymap = L.map("mapid", {
-	center: defaultLatlng,
+  center: defaultLatlng,
   zoom: 15,
-  zoomControl: false
+  zoomControl: false,
 })
 const myIcon = L.icon({
   iconUrl: "./images/icon-location.svg",
   iconSize: [46, 56],
   iconAnchor: [23, 56],
 })
-// L.control({position: 'bottomleft'})
+L.control.zoom({ position: "bottomleft" }).addTo(mymap)
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-	attribution:
-		'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(mymap)
 
 const findLoc = async query => {
-	const loc = await fetch(baseURL + query)
+  const loc = await fetch(baseURL + query)
   const data = await loc.json()
   const position = L.latLng(data.location.lat, data.location.lng)
-  
+
   mymap.panTo(position)
   L.marker(position, { icon: myIcon }).addTo(mymap)
 
-  ip.innerText = data.ip 
-  city.innerText = data.location.city 
-  tz.innerText = 'UTC ' + data.location.timezone 
-  isp.innerText = data.isp 
+  ip.innerText = data.ip
+  city.innerText = data.location.city
+  tz.innerText = "UTC " + data.location.timezone
+  isp.innerText = data.isp
 }
 
 form.onsubmit = e => {
-	let query = ""
-	e.preventDefault()
-	query = "&domain=" + e.target[0].value + "&ipAddress=" + e.target[0].value
-	findLoc(query)
+  let query = ""
+  e.preventDefault()
+  query = "&domain=" + e.target[0].value + "&ipAddress=" + e.target[0].value
+  findLoc(query)
 }
 
 window.onload = findLoc("")
